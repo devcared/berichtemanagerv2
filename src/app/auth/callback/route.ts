829cb48f3ca499f1d745240ec50ee11c7f574a9d
Emil Schröder <123828029+devcared@users.noreply.github.com>
2026-03-16 13:55:01 +0100

    Update route.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -11,8 +11,8 @@ export async function GET(request: Request) {
     const cookieStore = await cookies()
 
     const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://coxlhpllceqcuyufgvml.supabase.co',
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY || 'sb_publishable_AbqW2HHuLvenz7MlabX1Yw_LIYURdz7',
       {
         cookies: {
           getAll() {
